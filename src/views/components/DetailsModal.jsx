f191e35423de846934fd88c4031e7cd92c677242
Carine <86265525+Carinebs@users.noreply.github.com>
2023-05-03 22:12:22 -0300

    -Criando componente para detalhe

--- src/views/components/DetailsModal.jsx
+++ src/views/components/DetailsModal.jsx
@@ -1,5 +1,10 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  maskCardNumber,
+  formatAdress,
+  formatDate,
+} from "../../utils/functions";
 import {
   Button,
   Modal,
@@ -12,105 +17,122 @@ import {
   NavLink,
 } from "reactstrap";
 
+const DetailsModal = ({ isOpen, toggleModalDetails, dataDetails }) => {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get(
+          `https://fakerapi.it/api/v1/credit_cards?_quantity=3`
+        );
+        setData(response.data);
 
-const DetailsModal = ({ isOpen, toggleModalDetails }) => {
+        getData();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, []);
+
+  const { id, firstname, lastname, birthday, email, phone, address } =
+    dataDetails ? dataDetails : {};
   return (
-    <Modal isOpen={isOpen} toggle={toggleModalDetails} size="xl">
-      <ModalHeader toggle={toggleModalDetails}>
-        Visualizar o Cliente
-      </ModalHeader>
-      <ModalBody>
-        <Row className="d-flex align-items-center justify-content-center">
-          <Col className="row">
-            <span>
-              <strong>Nome Completo</strong>
-            </span>
-            <span>Carine Barros</span>
-          </Col>
-          <Col className="row">
-            <span>
-              <strong>ID</strong>
-            </span>
-            <span>0</span>
-          </Col>
-        </Row>
-        <Row className="d-flex align-items-center justify-content-center">
-          <Col className="row">
-            <span>
-              <strong>Email</strong>
-            </span>
-            <span>krinecbs@gmail.com</span>
-          </Col>
-          <Col className="row">
-            <span>
-              <strong>Data de Nascimento</strong>
-            </span>
-            <span>31/05/2000</span>
-          </Col>
-        </Row>
-        <Row className="d-flex align-items-center justify-content-center">
-          <Col className="row">
-            <span>
-              <strong>Telefone</strong>
-            </span>
-            <span>+55 (83) 99999-9999</span>
-          </Col>
-          <Col className="row">
-            <span>
-              <strong>Endereço</strong>
-            </span>
-            <span>Rua blabla, 99, bairro, País</span>
-          </Col>
-        </Row>
-        <Row style={{ paddingTop: "0.3rem" }}>
-          {" "}
-          <h5>Cartões de crédito</h5>
-        </Row>
-        <Row style={{ padding: "1rem" }}>
-          <Table bordered style={{ padding: "1rem" }}>
-            <thead>
-              <tr>
-                <th>Nome no cartão</th>
-                <th>Número</th>
-                <th>Validade</th>
-                <th>Bandeira</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </Table>
-        </Row>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="secondary" onClick={toggleModalDetails}>
-          Cancelar
-        </Button>
+    dataDetails && (
+      <Modal isOpen={isOpen} toggle={toggleModalDetails} size="xl">
+        <ModalHeader toggle={toggleModalDetails}>
+          Visualizar o Cliente
+        </ModalHeader>
+        <ModalBody>
+          <Row className={"d-flex align-items-center justify-content-center"}>
+            <Col className={"row"}>
+              <span>
+                <strong>Nome Completo</strong>
+              </span>
+              <span>{`${firstname} ${lastname}`}</span>
+            </Col>
+            <Col className={"row"}>
+              <span>
+                <strong>ID</strong>
+              </span>
+              <span>{id ?? ""}</span>
+            </Col>
+          </Row>
+          <Row className={"d-flex align-items-center justify-content-center"}>
+            <Col className="row">
+              <span>
+                <strong>Email</strong>
+              </span>
+              <span>{email ?? ""}</span>
+            </Col>
+            <Col className={"row"}>
+              <span>
+                <strong>Data de Nascimento</strong>
+              </span>
+              <span>{formatDate(birthday ?? "") ?? ""}</span>
+            </Col>
+          </Row>
+          <Row className={"d-flex align-items-center justify-content-center"}>
+            <Col className={"row"}>
+              <span>
+                <strong>Telefone</strong>
+              </span>
+              <span>{phone ?? ""}</span>
+            </Col>
+            <Col className={"row"}>
+              <span>
+                <strong>Endereço</strong>
+              </span>
+              <span>{formatAdress(address ?? "") ?? ""}</span>
+            </Col>
+          </Row>
+          <Row style={{ paddingTop: "0.3rem" }}>
+            {" "}
+            <h5>Cartões de crédito</h5>
+          </Row>
+          <Row style={{ padding: "1rem" }}>
+            <Table bordered style={{ padding: "1rem" }}>
+              <thead>
+                <tr>
+                  <th>Nome no cartão</th>
+                  <th>Número</th>
+                  <th>Validade</th>
+                  <th>Bandeira</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data &&
+                  data.data &&
+                  data.data.map((registro, index) => {
+                    const { id, owner, number, expiration, type } = registro;
+                    return (
+                      <tr key={id}>
+                        <td>{owner}</td>
+                        <td>{maskCardNumber(number)}</td>
+                        <td>{expiration}</td>
+                        <td>{type}</td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
+          </Row>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggleModalDetails}>
+            Cancelar
+          </Button>
 
-        <NavLink
-          href="https://static.snxpay.com/pdf/Fatura%20Elevaty.pdf"
-          target="_blank"
-          style={{ textDecoration: "none" }}
-        >
-          <Button color="primary">Visualizar PDF</Button>
-        </NavLink>
-      </ModalFooter>
-    </Modal>
+          <NavLink
+            href="https://static.snxpay.com/pdf/Fatura%20Elevaty.pdf"
+            target="_blank"
+            style={{ textDecoration: "none" }}
+          >
+            <Button color="primary">Visualizar PDF</Button>
+          </NavLink>
+        </ModalFooter>
+      </Modal>
+    )
   );
 };
 
